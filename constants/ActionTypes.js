4b1033352a59de34137262b4a5f7fd12d3b107fd
jk0 <wjkjyp@gmail.com>
2016-07-18 02:54:14 +0800

    ActionType constants

--- constants/ActionTypes.js
+++ constants/ActionTypes.js
@@ -1,4 +1,11 @@
-export const ADD_UNIT = 'ADD_UNIT';
-export const MAKE_UNIT_LIVE = 'MAKE_UNIT_LIVE';
-export const MAKE_UNIT_GROW = 'MAKE_UNIT_GROW';
-export const MAKE_UNIT_DIE = 'MAKE_UNIT_DIE';
+export const INIT_UNITS = 'INIT_UNITS';
+export const UPDATE_UNIT_STATE = 'UPDATE_UNIT_STATE';
+export const UPDATE_UNITS_STATE = 'UPDATE_UNITS_STATE';
+export const GAME_BEGIN = 'GAME_BEGIN';
+export const GAME_END = 'GAME_END';
+
+export const UnitStates = {
+  YOUNG: 'YOUNG',
+  OLD: 'OLD',
+  DEAD: 'DEAD',
+}
