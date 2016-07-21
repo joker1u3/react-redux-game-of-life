6df4e249366e9b924581c80cd5da87e7ab45013e
jk0 <wjkjyp@gmail.com>
2016-07-21 18:06:52 +0800

    Add attr editing.

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
 
-const Toolbar = ({ generation, actions }) => (
+const Toolbar = ({ generation, editing, actions }) => (
   <div className='toolbar'>
     <div className='toolbar-item'>Generation: {generation}</div>
     <button className='toolbar-item' onClick={actions.gameInit}>New</button>
     <button className='toolbar-item' onClick={actions.gameStart}>Start</button>
     <button className='toolbar-item' onClick={actions.gameStop}>Stop</button>
+    <div className='toolbar-item'>Editing: {editing.toString()}</div>
   </div>
 )
 
