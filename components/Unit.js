d2739202b38a820008a7aee2037ebdbba0395d78
jk0 <wjkjyp@gmail.com>
2016-07-21 18:08:27 +0800

    Disabled onClick method.

--- components/Unit.js
+++ components/Unit.js
@@ -2,22 +2,22 @@ import React, { PropTypes } from 'react';
 import { UnitStates } from '../constants/ActionTypes';
 import classnames from 'classnames';
 
-const Unit = ({ onClick, state, editing }) => (
+const Unit = ({ onClick, state, disabled }) => (
   <div
-    onClick={ onClick }
+    onClick={ disabled?'':onClick }
     className={classnames(
       { young: state === UnitStates.YOUNG },
       { old: state === UnitStates.OLD },
       { dead: state === UnitStates.DEAD },
       'unit'
-    )}>
+    )}
+    >
   </div>
 )
 
 Unit.propTypes = {
   onClick: PropTypes.func.isRequired,
-  state: PropTypes.string.isRequired,
-  editing: PropTypes.bool.isRequired
+  state: PropTypes.string.isRequired
 };
 
 export default Unit;
