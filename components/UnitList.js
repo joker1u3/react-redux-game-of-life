8d82f9fe94d3a4be5107068ce0b5ffaf45d4c944
jk0 <wjkjyp@gmail.com>
2016-07-21 14:23:58 +0800

    Components should be stateless.

--- components/UnitList.js
+++ components/UnitList.js
@@ -2,25 +2,21 @@ import React, { Component, PropTypes } from 'react';
 import Unit from './Unit';
 import { UnitStates } from '../constants/ActionTypes';
 
-export default class UnitList extends Component {
-  render() {
-    return (
-      <div className='matrix'>
-        {this.props.units.map((unitsColumn, index) =>
-          <div className='row' key={ `row_${index}` } >
-            {unitsColumn.map((unit, columnIndex) =>
-                <Unit
-                  {...unit}
-                  key={ `unit_${index}_${columnIndex}` }
-                  onClick={() => this.props.onUnitClick(unit.id, UnitStates.YOUNG)}
-                />
-            )}
-          </div>
+const UnitList = ({ onUnitClick, units }) => (
+  <div className='matrix'>
+    {units.map((unitsColumn, index) =>
+      <div className='row' key={ `row_${index}` } >
+        {unitsColumn.map((unit, columnIndex) =>
+            <Unit
+              {...unit}
+              key={ `unit_${index}_${columnIndex}` }
+              onClick={() => onUnitClick(unit.id, UnitStates.YOUNG)}
+            />
         )}
       </div>
-    )
-  }
-}
+    )}
+  </div>
+)
 
 UnitList.propTypes = {
   onUnitClick: PropTypes.func.isRequired,
@@ -33,3 +29,5 @@ UnitList.propTypes = {
     ).isRequired
   ).isRequired
 }
+
+export default UnitList;
