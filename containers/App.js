6aa1887c54aebc98deff8701a1af483838736440
jk0 <wjkjyp@gmail.com>
2016-07-21 20:15:11 +0800

    Render App by running condition. Add attr running to rootState. Add attr running to App.propTypes.

--- containers/App.js
+++ containers/App.js
@@ -3,27 +3,46 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import UnitList from '../components/UnitList';
 import Toolbar from '../components/Toolbar';
+import Timer from './Timer';
 import * as UnitActions from '../actions';
 import { UnitStates } from '../constants/ActionTypes';
 
 export default class App extends Component {
   render() {
-    const { visibleUnits, generation, editing, actions, toolbarActions } = this.props;
-    console.log(editing);
-    return (
-      <div>
-        <Toolbar
-          generation={generation}
-          actions={toolbarActions}
-          editing={editing}
-          />
-        <UnitList
-          units={visibleUnits}
-          editing={editing}
-          actions={actions}
-          />
-      </div>
-    );
+    const { visibleUnits, generation, editing, running, actions, toolbarActions } = this.props;
+    let app;
+    if(running) {
+      app = (
+        <Timer update={actions.updateUnitsState} updateInterval='200'>
+          <Toolbar
+            generation={generation}
+            actions={toolbarActions}
+            editing={editing}
+            />
+          <UnitList
+            units={visibleUnits}
+            editing={editing}
+            actions={actions}
+            />
+        </Timer>
+      );
+    } else {
+      app = (
+        <div>
+          <Toolbar
+            generation={generation}
+            actions={toolbarActions}
+            editing={editing}
+            />
+          <UnitList
+            units={visibleUnits}
+            editing={editing}
+            actions={actions}
+            />
+        </div>
+      );
+    }
+    return app;
   }
 }
 
@@ -38,6 +57,7 @@ App.propTypes = {
   ).isRequired,
   generation: PropTypes.number.isRequired,
   editing: PropTypes.bool.isRequired,
+  running: PropTypes.bool.isRequired,
   actions: PropTypes.object.isRequired,
   toolbarActions: PropTypes.object.isRequired
 }
@@ -46,7 +66,8 @@ function select(state) {
   return {
     visibleUnits: state.unitList.units,
     generation: state.unitList.generation,
-    editing: state.unitList.editing
+    editing: state.unitList.editing,
+    running: state.unitList.running
   }
 }
 
