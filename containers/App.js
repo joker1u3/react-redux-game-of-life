541dc6d7faa3c1c320b12d8d18f66f4ba4df7d89
jk0 <wjkjyp@gmail.com>
2016-07-21 14:47:25 +0800

    Replace this.props.visibleUnits with const visibleUnits.

--- containers/App.js
+++ containers/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <div>
         <UnitList
-          units={this.props.visibleUnits}
+          units={visibleUnits}
           actions={actions}
           />
       </div>
